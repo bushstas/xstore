140e0c32362778c40be9f139d1ed0cd066166f9e
Bushmakin Stas <bushstas@mail.ru>
2017-10-31 20:56:24 +0500

    refactored

--- bin/template.js
+++ bin/template.js
@@ -1,5 +1,38 @@
-export default {
-	reducer: () => {
+import axios from 'axios'
+
+const DEFAULT_STATE = {
+	name: 'user',
+	status: 'alive'
+}
+
+const load = (dispatch, payload) => {
+	axios.get('/api/load.php', payload)
+		.then(({data}) => {
+			dispatch('user_set', data);
+		});
+}
+
+const reducer = (state = DEFAULT_STATE, action) => {
+	let {type, payload} = action;
+
+	switch (type) {
+		case 'change_status':
+			return {
+				...state,
+				status: payload.status
+			}
+
+		case 'set':
+			return {
+				...state,
+				...payload
+			}
 
 	}
+	return state;
+}
+
+export default {
+	load,
+	reducer
 }
